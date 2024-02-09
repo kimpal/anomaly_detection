20850ba05e4dea92e466213ff62996bde6b8dc80
Kimpal <56063627+kimpal@users.noreply.github.com>
2024-02-09 11:00:15 +0100

    Removed some code and  updated to work on Ton_iot and Bot_iot

--- anomaly-detection-main/XboostFeatureselection_multipel_binary_clasifyer_and_multi_classifyer/XBoost_Feature_selection_multiclass_and_binary_models_automated_training.py
+++ anomaly-detection-main/XboostFeatureselection_multipel_binary_clasifyer_and_multi_classifyer/XBoost_Feature_selection_multiclass_and_binary_models_automated_training.py
@@ -1,4 +1,4 @@
-# Code to run multiple models on different datasets and whit gridsearch and set parameters
+# Code to run multiple models on different datasets and with gridsearch and set parameters
 import sys
 import time
 import pandas as pd
@@ -19,14 +19,23 @@ print("Loading dataset...")
 # datasets defined for multiclass and binary
 #UNSW_NB15_train_dataset, UNSW_NB15_val_dataset, UNSW_NB15_test_dataset = "../Dataset/train_1_pp3_multi.csv", "../Dataset/val_pp3_multi.csv", "../Dataset/test_pp3_multi.csv"
 #UNSW_NB15_train_binary_dataset, UNSW_NB15_val_binary_dataset, UNSW_NB15_test_binary_dataset = "../Dataset/train_1_pp3_binary.csv", "../Dataset/val_pp3_binary.csv", "../Dataset/test_pp3_binary.csv"
-TON_IoT_train, TON_IoT_val, TON_IoT_test = '../Dataset/Ton_IoT/train_pp_multi.csv', '../Dataset/Ton_IoT/val_pp_multi.csv', '../Dataset/Ton_IoT/test_pp_multi.csv'
+#TON_IoT_train, TON_IoT_val, TON_IoT_test = '../Dataset/Ton_IoT/train_pp_multi.csv', '../Dataset/Ton_IoT/val_pp_multi.csv', '../Dataset/Ton_IoT/test_pp_multi.csv'
 #TON_IoT_train_binary, TON_IoT_val_binary, TON_IoT_test_binary = '../Dataset/Ton_IoT/train_pp_binary.csv', '../Dataset/Ton_IoT/val_pp_binary.csv', '../Dataset/Ton_IoT/test_pp_binary.csv'
-#Bot_IoT_train, Bot_IoT_val, Bot_IoT_test = '../Dataset/BoT-IoT/train_pp_multi.csv','../Dataset/BoT-IoT/val_pp_multi.csv', '../Dataset/BoT-IoT/test_pp_multi.csv'
-#Bot_IoT_train_binary, Bot_IoT_val_binary, BoT_IoT_test_binary = '../Dataset/BoT-IoT/train_pp_binary.csv','../Dataset/BoT-IoT/val_pp_binary.csv', '../Dataset/BoT-IoT/test_pp_binary.csv'
+Bot_IoT_train, Bot_IoT_val, Bot_IoT_test = '../Dataset/BoT-IoT/train_pp_multi.csv','../Dataset/BoT-IoT/val_pp_multi.csv', '../Dataset/BoT-IoT/test_pp_multi.csv'
+#Bot_IoT_train_binary, Bot_IoT_val_binary, Bot_IoT_test_binary = '../Dataset/BoT-IoT/train_pp_binary.csv','../Dataset/BoT-IoT/val_pp_binary.csv', '../Dataset/BoT-IoT/test_pp_binary.csv'
 
-train = pd.read_csv(TON_IoT_train)
-val = pd.read_csv(TON_IoT_val)
-test = pd.read_csv(TON_IoT_test)
+#train = pd.read_csv(TON_IoT_train)
+#val = pd.read_csv(TON_IoT_val)
+#test = pd.read_csv(TON_IoT_test)
+#train = pd.read_csv(TON_IoT_train_binary)
+#val = pd.read_csv(TON_IoT_val_binary)
+#test = pd.read_csv(TON_IoT_test_binary)
+train = pd.read_csv(Bot_IoT_train)
+val = pd.read_csv(Bot_IoT_val)
+test = pd.read_csv(Bot_IoT_test)
+#train = pd.read_csv(Bot_IoT_train_binary)
+#val = pd.read_csv(Bot_IoT_val_binary)
+#test = pd.read_csv(BoT_IoT_test_binary)
 #train = pd.read_csv(UNSW_NB15_train_dataset)
 #val = pd.read_csv(UNSW_NB15_val_dataset)
 #test = pd.read_csv(UNSW_NB15_test_dataset)
@@ -34,9 +43,9 @@ test = pd.read_csv(TON_IoT_test)
 # list of respective target values on different dataset dataset
 #target_value = "attack_cat" # target values on Multiclass UNSW_NB15
 #target_value = "label" # target values on Binary UNSW_NB15
-target_value = "type" # target values on Multiclass TON_IoT
+#target_value = "type" # target values on Multiclass TON_IoT
 #target_value = "label" # target values on Binary TON_IoT
-#target_value = "category" # target values on Multiclass Bot-IoT
+target_value = "category" # target values on Multiclass Bot-IoT
 #target_value = "attack" # target values on Binary Bot-IoT
 x_train, y_train = train.drop([target_value], axis=1), train[target_value]
 x_val, y_val = val.drop([target_value], axis=1), val[target_value]
@@ -51,14 +60,23 @@ print("Original shape x_train:",'\t', x_train.shape)
 print("Original shape x_val:",'\t', x_val.shape)
 print("Original shape x_test:",'\t', x_test.shape)
 
-
+#binary_TON_IoT_threshold = 0.0044252956
+#multiclass_TON_IoT_threshold = 0.007512621
+#Multiclass_BOT_IoT_threshold = 0.026606342 # 7 features # used
+#binary_BOT_IoT_threshold = 0.03815346 #11 features since the original is 16
+#Binary_BOT_IoT_threshold = 0.03753139 # 12 features
+#Binary_BOT_IoT_threshold = 0.042484876 # 10 features
+#Binary_BOT_IoT_threshold = 0.050731182 # 8 features
+#Binary_BOT_IoT_threshold = 0.054992266 # 7 features # used
+#0.026790911
+#0.061215572
 #"""
-print("FeatureSelection whit XGBoost starts...")
-# Feature selection on Binary data whit XGBoost
+print("FeatureSelection with XGBoost starts...")
+# Feature selection on Binary data with XGBoost
 start_time_select_features = time.time()
 def select_features(x_train_inner, y_train_inner, x_test_inner, x_val_inner):
 	# configured to select a subset of features the XGBoost
-	fs_inner = SelectFromModel(XGBClassifier(), threshold=0.007512621)
+	fs_inner = SelectFromModel(XGBClassifier(), threshold=0.026606342)
 	# learn relationship from training data
 	fs_inner.fit(x_train_inner, y_train_inner)
 	# transform train input data
@@ -213,60 +231,3 @@ def evaluate_models(x_train_input=x_train, y_train_input=y_train, x_val_input=x_
 # Assuming x_train, y_train, x_val, y_val, x_test, y_test, x_train_fs, x_val_fs, x_test_fs is defined
 #evaluate_models(x_train_input=x_train, y_train_input=y_train, x_val_input=x_val, y_val_input=y_val, x_test_input=x_test, y_test_input=y_test, models=models_to_evaluate, x_train_fs_input=x_train_fs, x_val_fs_input=x_val_fs, x_test_fs_input=x_test_fs):
 evaluate_models(x_train, y_train, x_val, y_val, x_test, y_test, models_to_evaluate, x_train_fs, x_val_fs, x_test_fs)
-
-
-"""
-# simpel evaluation whit out gir search could be used when best param in grid search is found
-def evaluate_models(x_train, y_train, x_val, y_val, x_test, y_test, models):
-	for model_name, model, params in models:
-		print(f"Training {model_name} with parameters: {params}")
-
-		# Measure training time
-		start_time = time.time()
-		model.fit(x_train, y_train)
-		training_time = time.time() - start_time
-
-		# Evaluate on the training set
-		y_train_pred = model.predict(x_train)
-		print_metrics("Training", y_train, y_train_pred)
-
-		# Evaluate on validation set
-		y_val_pred = model.predict(x_val)
-		print_metrics("Validation", y_val, y_val_pred)
-
-		# Evaluate on the test set
-		y_test_pred = model.predict(x_test)
-		print_metrics("Test", y_test, y_test_pred)
-
-		# Print training time
-		print(f"Training time for {model_name}: {training_time:.4f} seconds\n")
-
-# Random forest parameters
-params = {
-	"criterion": "entropy",
-	"bootstrap": True,
-	"n_estimators": 20,
-	"max_depth": 50,
-	"min_samples_split": 2,
-	"min_samples_leaf": 1,
-	"n_jobs": -1
-}
-
-# Create a RandomForestClassifier instance with the specified parameters
-custom_rf_model = RandomForestClassifier(**params)
-
-# Example Usage
-models_to_evaluate = [
-	("XGBoost", XGBClassifier(), {}),
-   # ("Custom RandomForest", custom_rf_model, params),
-	("DT", DecisionTreeClassifier(max_depth=2), {}),
-	#("custom SVM",SVC(kernel='rbf',gamma='scale', degree=3, C=1.12), {}),
-	#("KNN",KNeighborsClassifier(n_neighbors=3,), {}),
-	#("LR",LogisticRegression(max_iter = 2000, random_state=10), {}),
-	#("ANN",MLPClassifier(hidden_layer_sizes=(5,),solver='adam',learning_rate='adaptive', learning_rate_init=0.02,), {}),
-	#("SVM", SVC(), {'C': 1.0, 'kernel': 'rbf'}),
-]
-
-# Assuming you have x_train, y_train, x_val, y_val, x_test, y_test defined
-evaluate_models(x_train, y_train, x_val, y_val, x_test, y_test, models_to_evaluate)
-"""
