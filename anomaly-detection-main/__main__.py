fe13c0ee52119160ccfac6dcbfd9b3cd9b91bc4b
Aleksander V Eriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2024-06-15 13:37:53 +0200

    Delete anomaly-detection-main directory

--- anomaly-detection-main/__main__.py
+++
@@ -1,9 +0,0 @@
-# this is the mian.py file the intention is to automating the start of the preprocessing of the data
-# this file should essentially only cals the functions in: 
-# "pr-processing (Complet).py", 
-# Pre-Processing_for_multiclass.py and 
-# pre-Processing_MultiClass_on_Hot_encod.py
-# this way the all the preprocessing can start whit just running one file 
-# when running up the Docker container or just runing manualy.
-
-#import_train_test()
